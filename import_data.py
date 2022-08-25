529c2a9c25c257aa8d76d96960d0f343f7294db1
Chris Hunt <chris.hunt@hudl.com>
2022-08-25 08:28:12 +0100

    feat: add import script

---
+++ import_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
+                'Acceleration', 'Model Year', 'Origin']
+
+raw_dataset = pd.read_csv(url, names=column_names,
+                          na_values='?', comment='\t',
+                          sep=' ', skipinitialspace=True)
+
+
+raw_dataset.to_csv("auto_mpg.csv", index=False)
